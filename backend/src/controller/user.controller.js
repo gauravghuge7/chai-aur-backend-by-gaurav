36200c6bb36425e03c22181db9520c5e8093b3dc
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-09 23:14:26 +0530

    register user incmoplete

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,14 +1,57 @@
 
+import { ApiError } from '../utils/ApiError.js';
 import {asyncHandler } from '../utils/asyncHandler.js'
-
-
+import {User } from "../models/user.model.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
 
+   // get user detail from frontend
+   // validation not empty
 
-   return res
-   .status(200)
-   .json({message: 'Register User'})
+   // check user already exist or not
+
+   const {username, fullName, email, password, } = req.body;
+   
+
+   if(
+      [username, fullName, email, password].some(item => item?.trim() === "")
+   ){
+      throw new ApiError(400, "Please fill all the fields");
+   }
+
+   const existingUser = await User.findOne({
+      $or: [{ username }, { email }]
+   })
+
+   if(existingUser){
+      throw new ApiError(409, "User already exist in the system");
+   }
+
+   const avatarLocalPath = req.files?.avatar[0]?.path;
+
+   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+   if(!avatarLocalPath) {
+      throw new ApiError(400, "Please upload avatar");
+   }
+
+   const avatar = await uploadOnCloudinary(avatarLocalPath);
+   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+   if(!avatar) {
+      throw new ApiError(400, "Please upload avatar");
+   }
+
+   const user = await User.create({
+      fullName,
+      avatar: avatar?.url || "",
+      coverImage: coverImage?.url || "",
+      username: username.toLowerCase(),
+      email,
+      password,
+
+   })
 
 })
 
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,14 +1,26 @@
 import {Router} from 'express'
-
+import { upload } from '../middlewares/multer.middleware.js';
 const router = Router();
 
 
 import { registerUser } from '../controller/user.controller.js';
 
 
+router.route('/register').post(
+
+   upload.fields([
+      {
+         name: "avatar",
+         maxCount: 1
+      },
+      {
+         name: "coverImage",
+         maxCount: 2
+      }
+   ]),
+   registerUser
+);
 
 
 
-
-export default router
-
+export default router;
